766463059482dfea81d2f0b2aabf51d64c07cf30
StanRavenman <sagoth6@gmail.com>
2019-12-09 01:31:44 +0200

    2_4 model.Account,Article,Currency,Transaction,Transfer

---
+++ src/personalfinance/model/Currency.java
@@ -0,0 +1,106 @@
+package personalfinance.model;
+
+import personalfinance.exception.ModelException;
+
+import java.util.Objects;
+
+public class Currency extends Common {
+
+    private String title;
+    private String code;
+    private double rate;
+    private boolean on;
+    private boolean base;
+
+    public Currency() {}
+
+    public Currency(String title, String code, double rate, boolean on, boolean base) throws ModelException {
+        if (title.length() == 0) throw new ModelException(ModelException.TITLE_EMPTY);
+        if (code.length() == 0) throw new ModelException(ModelException.CODE_EMPTY);
+        if (rate <= 0) throw new ModelException(ModelException.RATE_INCORRECT);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public boolean isBase() {
+        return base;
+    }
+
+    public void setBase(boolean base) {
+        this.base = base;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" + "title=" + title + ", code=" + code + ", rate=" + rate + ", isOn=" + on + ", isBase=" + base + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.code);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Currency other = (Currency) obj;
+        if (!Objects.equals(this.code, other.code)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getValueForComboBox() {
+        return title;
+    }
+
+    public double getRateByCurrency(Currency currency) {
+        return rate / currency.rate;
+    }
+
+
+
+
+
+}
