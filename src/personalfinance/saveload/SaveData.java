adb35f48e3e9d2e0d44c8e0bb69220e4ea3fa318
StanRavenman <sagoth6@gmail.com>
2019-12-09 12:27:46 +0200

    3_2 saveload.SaveLoad, saveload.SaveData

--- src/personalfinance/saveload/SaveData.java
+++ src/personalfinance/saveload/SaveData.java
@@ -66,7 +66,23 @@ public final class SaveData {
         return new Currency();
     }
 
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 
+    public void setCurrencies(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
 
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
 
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setTransfers(List<Transfer> transfers) {
+        this.transfers = transfers;
+    }
 }
