2918f2fc9652c8bbb933f40a0b3089f82fcbfeaa
StanRavenman <sagoth6@gmail.com>
2019-12-11 19:19:37 +0200

    5_1 gui.dialog.ErrorDialog

---
+++ src/personalfinance/gui/dialog/ErrorDialog.java
@@ -0,0 +1,14 @@
+package personalfinance.gui.dialog;
+
+import javax.swing.JOptionPane;
+import personalfinance.gui.MainFrame;
+import personalfinance.settings.Text;
+
+
+public class ErrorDialog {
+
+    public static void show(MainFrame frame, String text) {
+        JOptionPane.showMessageDialog(frame, Text.get(text), Text.get("ERROR"), JOptionPane.ERROR_MESSAGE);
+    }
+
+}
